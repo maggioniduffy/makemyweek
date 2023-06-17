3e5e2a200ce74b6161d51f3d5d566b67f104253e
maggioniduffy <faustino.maggioni@est.fi.uncoma.edu.ar>
2023-06-16 21:01:29 -0300

    calendar waiting mockup

--- src/pages/result.tsx
+++ src/pages/result.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 import Head from "next/head";
 import { Inter } from "next/font/google";
+import Link from "next/link";
+import { useRouter } from "next/router";
 const inter = Inter({ subsets: ["latin"] });
+
+const WhiteSpace = () => <div className="bg-transparent h-full w-full"></div>;
+
+const heights = ["12", "16", "20", "24"];
+const GraySpace = () => {
+  const i = Math.floor(Math.random() * 4);
+  const h = heights[i];
+  return (
+    <div className={`bg-gray rounded-lg h-${h} m-2 bg-opacity-50 w-full`}></div>
+  );
+};
 
 const result = () => {
   return (
@@ -11,16 +24,110 @@ const result = () => {
         <meta name="Creador de calendario semanal. Organiza tus clases, entrenamientos, cursos o lo que necesites" />
       </Head>
       <header className="fixed py-4 z-50 bg h-fit text-center w-full">
-        <h1 className="text-2xl text-lightpink font-medium text-shadow">
+        <Link
+          className="text-2xl text-lightpink font-medium text-shadow"
+          href={"/"}
+        >
           {" "}
           Make My Week{" "}
-        </h1>
+        </Link>
       </header>
       <main
         className={`flex h-screen w-screen flex-col items-center justify-between ${inter.className}`}
       >
-        <table className="bg-white w-10/12 rounded-xl shadow h-full m-48">
-          {" "}
+        <table className="table-fixed bg-white w-10/12 rounded-xl shadow h-full m-48 p-4">
+          <thead className="">
+            {/* <tr className="text-gray">
+              <th>Lunes</th>
+              <th>Martes</th>
+              <th>Miercoles</th>
+              <th>Jueves</th>
+              <th>Viernes</th>
+              <th>Sabado</th>
+            </tr> */}
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <GraySpace />
+              </td>
+              <td>
+                <WhiteSpace />
+              </td>
+              <td>
+                <GraySpace />
+              </td>
+              <td>
+                <WhiteSpace />
+              </td>
+              <td>
+                <GraySpace />
+              </td>
+              <td>
+                <WhiteSpace />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <WhiteSpace />
+              </td>
+              <td>
+                <GraySpace />
+              </td>
+              <td>
+                <GraySpace />
+              </td>
+              <td>
+                <WhiteSpace />
+              </td>
+              <td>
+                <GraySpace />
+              </td>
+              <td>
+                <WhiteSpace />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <WhiteSpace />
+              </td>
+              <td>
+                <WhiteSpace />
+              </td>
+              <td>
+                <GraySpace />
+              </td>
+              <td>
+                <WhiteSpace />
+              </td>
+              <td>
+                <GraySpace />
+              </td>
+              <td>
+                <WhiteSpace />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <WhiteSpace />
+              </td>
+              <td>
+                <WhiteSpace />
+              </td>
+              <td>
+                <GraySpace />
+              </td>
+              <td>
+                <WhiteSpace />
+              </td>
+              <td>
+                <GraySpace />
+              </td>
+              <td>
+                <WhiteSpace />
+              </td>
+            </tr>
+          </tbody>
         </table>
       </main>
     </div>
