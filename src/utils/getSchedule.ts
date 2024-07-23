7c51896ce30e10b5ff0bd9cd6ab1078d77357aed
maggioniduffy <fausmaggioni5@gmail.com>
2024-07-23 13:54:54 -0300

    getCalendar function made, could work => to test

--- src/utils/getSchedule.ts
+++ src/utils/getSchedule.ts
@@ -1,11 +1,16 @@
 import { Activity, Option, Turn } from "@/models";
 
-interface Respond {
-  score: number;
-  turns: { activity: Activity; priority: Option }[];
+interface ResTurn {
+  activity: Activity;
+  priority: Option;
 }
 
-function isCompatible(a: Turn, b: Turn) {
+interface Respond {
+  score: number;
+  turns: ResTurn[];
+}
+
+function isCompatible(a: Turn, b: Turn):boolean {
   const dayA = a.day;
   const startA = a.start;
   const endA = a.end;
@@ -31,32 +36,72 @@ function isCompatible(a: Turn, b: Turn) {
   return false;
 }
 
+function compareOptions(a: Option, b: Option):number {
+  let i = 0;
+  let res = true;
+  let len = a.turns.length;
+  const turnsA = a.turns
+  const turnsB = b.turns
+  while (i<len && res) {
+    let turnA = turnsA[i];
+
+    turnsB.forEach((turnB) => {
+      res = isCompatible(turnA,turnB);
+    })
+
+    i++;
+  }
+
+  if (res) {
+    return a.priority + b.priority
+  } else {
+    return -1;
+  }
+}
+
+
+
 export async function getCalendar(activities: Array<Activity>) {
   console.log("Acts", activities);
   let responses: Respond[] = []
-  let maxScoreAllowed = 999;
-  let firstAct = activities[0]
-  firstAct.options.forEach((o) => {
-    activities.slice(1).forEach((a) => {
-      o.turns.forEach((t) => {
-        a.options.forEach((ao) => {
-          let flag = true;
-          ao.turns.forEach((aot) => {
-            if (!isCompatible(aot,t)) {
-              flag = false;
-            }
+  const firstAct = activities.shift()
+  const lenRemainActs = activities.length;
+  const len = firstAct?.options.length;
+  let i = 0
+  if (len){
+    while (i<len) {
+      let option = firstAct.options[i]
+      let j = 0;
+      let mainScore = 0;
+      let resTurns: ResTurn[] = [{activity: firstAct,priority: option}]
+      while (j<lenRemainActs){
+        const act = activities[j]
+        const lenActOptions = act.options.length
+        let score = -1;
+        let k = 0;
+        while (score == -1 && k < lenActOptions) {
+          score = compareOptions(option,act.options[k])
+          k++
+        }
+        if (score > 0) {
+          mainScore += score;
+          resTurns.push({
+            activity: act,
+            priority: act.options[k-1]
           })
-          if (flag) {
-            let score = o.priority + ao.priority
-            if (score < maxScoreAllowed) {
-              responses.push({
-                score,
-                turns: []
-              })
-            }
-          }
-        })
+        }
+        j++
+      }
+      responses.push({
+        score: mainScore,
+        turns: resTurns
       })
-    })
-  })
+      i++
+    }
+  }
+
+  responses.sort((a,b) => a.score < b.score ? -1:1);
+  responses.slice(0,3)
+
+  return responses
 }
